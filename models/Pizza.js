d703e841c93619e81601855ca70efda5506ce2a7
box-monkey <jr.gasparin15@gmail.com>
2022-02-19 15:37:21 -0500

    section 1 complete

---
+++ models/Pizza.js
@@ -0,0 +1,24 @@
+// you are on 18.2.1
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+});
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
